ba9d7f5bacc325041dde752fcd48d1cb0ca6a772
Lance <mr.longzhentian@gmail.com>
2026-04-23 11:24:38 +0800

    fix: increase default max listeners for EventEmitter to prevent memory leaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
+import { EventEmitter } from 'node:events'
 import { fileURLToPath, URL } from 'node:url'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 import { remarkModifiedTime } from './remark-modified-time.js'
+
+// Increase the limit to 20 or more
+EventEmitter.defaultMaxListeners = 20
 
 // https://astro.build/config
 export default defineConfig({
